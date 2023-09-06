1beab7e422f1920b876cdd3f39be470da4464277
LeticiaMeloSilv <123740554+LeticiaMeloSilv@users.noreply.github.com>
2023-09-06 12:30:34 -0300

    Add files via upload

---
+++ aula07/modulo/tabuada.js
@@ -0,0 +1,35 @@
+/*********************************************************************
+ * Objetivo: Gerar o calculo de uma tabuada
+ * Data: 06/09/2023
+ * Nome: Letícia
+ * Versão: 1.0
+ *********************************************************************/
+
+const gerarTabuada = function (valorTabuada, valorInicial, valorFinal) {
+    let tabuada = valorTabuada
+    let inicio = valorInicial
+    let final = valorFinal
+    let resultado;
+
+    if (tabuada == '' || inicio == '' || final == '') {
+        console.log('!ERRO! É obrigatorio informar todos os valores.')
+    }
+    else if (isNaN(tabuada) || isNaN(inicio) || isNaN(final)) {
+        console.log('!ERRO! Não é possivel a entrada de dados em caracteres.')
+    }
+    else {
+        tabuada=Number(tabuada)
+        inicio = Number(inicio)
+        final = Number(final)
+
+        if (inicio > final) {
+            inicio = valorFinal
+            final = valorInicial
+        }
+        while (inicio <= final) {
+            resultado = tabuada * inicio;
+            console.log(`${tabuada} x ${inicio} = ${resultado}`)
+            contador++
+        }
+    }
+}
